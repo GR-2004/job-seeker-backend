c74d3dee82f1b2e676c0565b15a74fb7fbe3c02d
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-02-17 21:00:32 +0530

    creating application and job routes

--- src/app.js
+++ src/app.js
@@ -14,6 +14,15 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//import routes
+import userRouter from "./routes/user.route.js"
+import applicationRouter from "./routes/application.route.js"
+import jobRouter from "./routes/job.route.js"
 
+
+//routes declartion
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/applications", applicationRouter)
+app.use("/api/v1/jobs", jobRouter)
 
 export default app
