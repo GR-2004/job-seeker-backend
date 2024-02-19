5d4eb15da3b96e3eea446fc738a285a28a1efeb7
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-02-19 17:43:21 +0530

    added user model and middlewares

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,27 @@
-import multer from "multer";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt  from "jsonwebtoken";
+import {User} from "../models/user.model.js"
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, './public/temp')
-    },
-    filename: function(req, file, cb){
-        cb(null, file.originalname)
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+        if(!token) {
+            throw new ApiError(404, "unauthorized request")
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+        if(!user){
+            throw new ApiError(401, "Invalid Access Token");
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
     }
 })
-
-export const upload = multer({storage: storage})
---
+++ src/models/user.model.js
@@ -0,0 +1,99 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            require: [true, "Please provide your email"],
+            unique: true,
+            lowercase: true,
+            maxlength: [3, "name must contain at least 3 characters!"],
+            minlength: [10, "name cannot exceed 10 characters!"],
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            require: [true, "Please provide your email"],
+            validate: [validator.isEmail, "please provide a valid email"]
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        phone: {
+            type: Number,
+            required: [true, "please provide your phone number..."],
+            validate: [validator.isMobilePhone, "please provide a valid mobile number"]
+        },
+        password: {
+            type: String,
+            maxlength: [8, "name must contain at least 8 characters!"],
+            minlength: [32, "name cannot exceed 32 characters!"],
+        },
+        role: {
+            type: String,
+            required: [true, "please provide your role"],
+            enum: ['Job Seeker', "Employer"],
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps:true
+    }
+);
+
+//hashing the password
+userSchema.pre("save", async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//comparing the password
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+//generating access token
+userSchema.methods.generateAccessToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id,
+            fullname: this.fullname,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+//generating refresh token
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+export const User = mongoose.model("User", userSchema);
