f8df23050561f9cd27fbf8ad189773ac4ea12145
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-02-22 17:59:19 +0530

    added updateJob and deleteJob functionality

--- src/controllers/job.controller.js
+++ src/controllers/job.controller.js
@@ -50,13 +50,88 @@ const postJob = asyncHandler( async (req, res) => {
 })
 
 
-const getMyJob = asyncHandler(async (req, res) => {
-    try {
-        
+const updateJob = asyncHandler(async (req, res) => {
+    try {   
+        if(req.user.role === "Job Seeker"){
+            throw new ApiError(400, "Job seeker can not post a job")
+        }
+
+        const { id } = req.params
+        if(!id){
+            throw new ApiError(400, "please insert Job id")
+        }
+        const job = await Job.findById(id);
+        if(!job){
+            throw new ApiError(404, "job not found")
+        }
+        const updatedJob = await Job.findByIdAndUpdate(
+            id,
+            req.body,
+            {
+                new: true,
+                runValidators: true,
+                useFindAndModify: false
+            }
+        )
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, updatedJob, "job updated successfully")
+        )
     } catch (error) {
         throw new ApiError(500, error.message)
     }
 })
+
+const deleteJob = asyncHandler(async (req, res) => {
+    try {
+        if(req.user.role === "Job Seeker"){
+            throw new ApiError(400, "Job seeker can not post a job")
+        }
+        const { id } = req.params
+        if(!id){
+            throw new ApiError(400, "please insert Job id")
+        }
+        const job = await Job.findById(id);
+        if(!job){
+            throw new ApiError(404, "job not found")
+        }
+        
+        const deletedJob = await job.deleteOne()
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, deletedJob, "Job deleted successfully")
+        )
+
+    } catch (error) {
+        throw new ApiError(500, error.message)
+    }
+})
+
+const getMyPostedJobs = asyncHandler(async (req, res) => {
+    try {      
+        if(req.user.role === "Job Seeker"){
+            throw new ApiError(400, "Job seeker can not post a job")
+        }
+        const job = await Job.find({postedBy: req.user.fullname})
+
+        if(!job){
+            throw new ApiError(500, "something went wrong while fetching your posted jobs")
+        }
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(200, job, "successfully fetched your posted jobs")
+        )
+    } catch (error) {
+        throw new ApiError(500, error.message)
+    }
+})
+
 
 const getAllJobs = asyncHandler( async (req, res) => {
     try {
@@ -76,5 +151,7 @@ const getAllJobs = asyncHandler( async (req, res) => {
 export {
     getAllJobs,
     postJob,
-    getMyJob
+    getMyPostedJobs,
+    updateJob,
+    deleteJob
 }
--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -2,13 +2,19 @@ import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { 
     getAllJobs,
-    postJob
+    postJob,
+    getMyPostedJobs,
+    updateJob,
+    deleteJob
 } from "../controllers/job.controller.js";
 
 const router = Router();
 
 //secured routes
+router.route("/postJob").post(verifyJWT, postJob)
+router.route("/updatejob/:id").patch(verifyJWT, updateJob)
+router.route("/deleteJob/:id").delete(verifyJWT, deleteJob)
+router.route("/getMyPostedJobs").get(verifyJWT, getMyPostedJobs)
 router.route("/getAllJobs").get(verifyJWT, getAllJobs);
-router.route("/postjob").post(verifyJWT, postJob)
 
 export default router
