6ce5d63b35a3ccc60ef5c361ffa52aa3c9703939
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-09-15 18:42:18 +0530

    replace apiError to json response when sending error message in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,8 @@ const generateAccessAndRefereshTokens = async (userId) => {
 
     return { accessToken, refreshToken };
   } catch (error) {
-    throw new ApiError(500, error.message);
+    // throw new ApiError(500, error.message);
+    return res.status(500).json({message: error.message || "something went wrong while generating access and refresh tokens"})
   }
 };
 
@@ -35,15 +36,15 @@ const registerUser = asyncHandler(async (req, res) => {
     const { email, fullname, password, phone, role } = req.body;
 
     if (
-      [fullname, email, password, role].some(
-        (field) => field?.trim() === ""
-      )
+      [fullname, email, password, role].some((field) => field?.trim() === "")
     ) {
-      throw new ApiError(404, "all fields are required");
+      // throw new ApiError(404, "all fields are required");
+      return res.status(404).json({message: "all fields are required"})
     }
 
     if (!phone) {
-      throw new ApiError(400, "Invalid phone number!");
+      // throw new ApiError(400, "Invalid phone number!");
+      return res.status(400).json({message: "Invalid phone number!"})
     }
 
     const isUserExist = await User.findOne({
@@ -51,7 +52,8 @@ const registerUser = asyncHandler(async (req, res) => {
     });
 
     if (isUserExist) {
-      throw new ApiError(409, "user already exists");
+      // throw new ApiError(409, "user already exists");
+      return res.status(409).json({message: "user already exists"});
     }
 
     const user = await User.create({
@@ -67,105 +69,128 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 
     if (!createdUser) {
-      throw new ApiError(
-        500,
-        "something went wrong while registering the user"
-      );
+      // throw new ApiError(
+      //   500,
+      //   "something went wrong while registering the user"
+      // );
+      return res.status(500).json({message: "something went wrong while registering the user"})
     }
 
     return res
       .status(201)
       .json(new ApiResponse(200, createdUser, "User registered successfully"));
   } catch (error) {
-    throw new ApiError(500, error.message);
+    // throw new ApiError(500, error.message);
+    return (
+      res.status(500),
+      json({ message: error.message || "something went wrong" })
+    );
   }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password, role } = req.body;
+  try {
+    const { email, password, role } = req.body;
 
-  if ([email, password, role].some((field) => field.trim() === "")) {
-    throw new ApiError(400, "username or password is required");
-  }
+    if ([email, password, role].some((field) => field.trim() === "")) {
+      // throw new ApiError(400, "username or password is required");
+      return res
+        .status(400)
+        .json({ message: "username or password is required" });
+    }
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
-    throw new ApiError(404, "username not found");
-  }
+    if (!user) {
+      // throw new ApiError(404, "username not found");
+      return res.status(404).json({ message: "username not found" });
+    }
 
-  if(user.role !=  role){
-    throw new ApiError(404, "username with given role not found");
-  }
+    if (user.role != role) {
+      // throw new ApiError(404, "username with given role not found");
+      return res
+        .status(404)
+        .json({ message: "username with given role not found" });
+    }
 
-  const checkPassword = await user.isPasswordCorrect(password);
+    const checkPassword = await user.isPasswordCorrect(password);
 
-  if (!checkPassword) {
-    throw new ApiError(404, "Invalid password");
-  }
+    if (!checkPassword) {
+      // throw new ApiError(404, "Invalid password");
+      return res.status(404).json({ message: "Invalid Password" });
+    }
 
-  const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
-    user._id
-  );
-
-  const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
-
-  //only server can change our cookies , but cookies is visible for both frontend and backend
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-
-  //we can return multiple cookies
-  return res
-    .status(200)
-    .cookie("accessToken", accessToken, options)
-    .cookie("refreshToken", refreshToken, options)
-    .json(
-      new ApiResponse(
-        200,
-        {
-          user: loggedInUser,
-          accessToken,
-          refreshToken,
-        },
-        "User logged in successfully"
-      )
+    const { accessToken, refreshToken } = await generateAccessAndRefereshTokens(
+      user._id
     );
+
+    const loggedInUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    //only server can change our cookies , but cookies is visible for both frontend and backend
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    //we can return multiple cookies
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          {
+            user: loggedInUser,
+            accessToken,
+            refreshToken,
+          },
+          "User logged in successfully"
+        )
+      );
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: error.message || "something went wrong" });
+  }
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(
-    req.user._id,
-    {
-      $unset: {
-        refreshToken: 1, //this removes the field from document
+  try {
+    await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $unset: {
+          refreshToken: 1, //this removes the field from document
+        },
       },
-    },
-    {
-      new: true,
-    }
-  );
-
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-
-  return res
-    .status(200)
-    .clearCookie("accessToken", options)
-    .clearCookie("refreshToken", options)
-    .json(new ApiResponse(200, "user logout successfully"));
+      {
+        new: true,
+      }
+    );
+  
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+  
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json(new ApiResponse(200, "user logout successfully"));
+  } catch (error) {
+    return res.status(500).json({message: error.message || "something went wrong"})
+  }
 });
 
 const getCurrentUser = asyncHandler(async (req, res) => {
   try {
-
-    if(!req.user){
-        throw new ApiError(404, "user not fetched")
+    if (!req.user) {
+      // throw new ApiError(404, "user not fetched");
+      return res.status(404).json({message: "user not fetched"});
     }
     return res
       .status(200)
@@ -173,7 +198,8 @@ const getCurrentUser = asyncHandler(async (req, res) => {
         new ApiResponse(200, req.user, "current user fetched successfully")
       );
   } catch (error) {
-    throw new ApiError(500, error.message)
+    // throw new ApiError(500, error.message);
+    return res.status(500).json({message: error.message || "something went wrong"})
   }
 });
 
